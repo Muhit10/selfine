7867c615e2253f16270d773c5b5948f3def8989a
Muhit10 <muhithaq@gmail.com>
2024-10-07 13:00:48 +0600

    background image

---
+++ android/app/src/main/kotlin/com/devment/selfine/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.devment.selfine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/HomeScreen/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //------------body-----------
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/image/selfine_background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
